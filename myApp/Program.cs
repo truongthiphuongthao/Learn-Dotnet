259595091bdd33a1f6169d6248c5029847c53705
Thao <thaotruong199826@gmail.com>
2021-02-19 13:32:09 +0700

    how to write function swap that use ref and out

---
+++ myApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 4;
+            SwapRef(ref a, ref b);
+            Console.WriteLine("a: " + a + " b: " + b);
+            SwapOut(out a, out b);
+            Console.WriteLine("a: " + a + " b: " + b);
+        }
+       static void SwapRef(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void SwapOut(out int a, out int b)
+        {
+            a = 2;
+            b = 4;
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
