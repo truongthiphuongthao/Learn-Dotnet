0bddcb6e8254eab27c7fb31b91f0985e37139435
Thao <thaotruong199826@gmail.com>
2021-03-02 14:32:02 +0700

    write test case ranking test

--- RankingProject/Ranking.Tests/UnitTest1.cs
+++ RankingProject/Ranking.Tests/UnitTest1.cs
@@ -6,25 +6,40 @@ namespace Ranking.Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-        //
         [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
-        //
-        [Test]
+        // 1- excellent
         public void Should_Return_Excellent_With_Score_9()
         {
-          Assert.AreEqual("excellent", Ranking.Rank(9f));
+          Assert.AreEqual("excellent", Ranking.Rank(9));
         }
 
-      
+        [Test]
+        // 2 - good
+        public void Should_Return_Excellent_With_Score_7()
+        {
+          Assert.AreEqual("good", Ranking.Rank(7));
+        }
 
+        [Test]
+        // 3 - avarage
+        public void Should_Return_Excellent_With_Score_6()
+        {
+          Assert.AreEqual("average", Ranking.Rank(6));
+        }
+
+        [Test]
+        // 4 - poor
+        public void Should_Return_Excellent_With_Score_4()
+        {
+          Assert.AreEqual("poor", Ranking.Rank(4));
+        }
+
+        [Test]
+        // 5 - bad
+        public void Should_Return_Excellent_With_Score_1()
+        {
+          Assert.AreEqual("bad", Ranking.Rank(1));
+        }
 
     }
 }
