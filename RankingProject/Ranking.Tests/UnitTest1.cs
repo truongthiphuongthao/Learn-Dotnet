1bec8314627ffdaaa0422b44d093b74bfc29d5f6
Thao <thaotruong199826@gmail.com>
2021-03-02 17:34:59 +0700

    change the name of function

--- RankingProject/Ranking.Tests/UnitTest1.cs
+++ RankingProject/Ranking.Tests/UnitTest1.cs
@@ -15,28 +15,28 @@ namespace Ranking.Tests
 
         [Test]
         // 2 - good
-        public void Should_Return_Excellent_With_Score_7()
+        public void Should_Return_Good_With_Score_7()
         {
           Assert.AreEqual("good", Ranking.Rank(7));
         }
 
         [Test]
         // 3 - avarage
-        public void Should_Return_Excellent_With_Score_6()
+        public void Should_Return_Average_With_Score_6()
         {
           Assert.AreEqual("average", Ranking.Rank(6));
         }
 
         [Test]
         // 4 - poor
-        public void Should_Return_Excellent_With_Score_4()
+        public void Should_Return_Poor_With_Score_4()
         {
           Assert.AreEqual("poor", Ranking.Rank(4));
         }
 
         [Test]
         // 5 - bad
-        public void Should_Return_Excellent_With_Score_1()
+        public void Should_Return_Bad_With_Score_1()
         {
           Assert.AreEqual("bad", Ranking.Rank(1));
         }
